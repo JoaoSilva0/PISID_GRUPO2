79a8c1b1d65f8cc53a523727ba3811aa5fa29672
pedroferreira <pmacf@iscte-iul.pt>
2024-03-23 15:21:10 +0000

    bookmarks

--- MQTT/MQTT/src/MongoToMQTT.java
+++ MQTT/MQTT/src/MongoToMQTT.java
@@ -1,6 +1,8 @@
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.UpdateOptions;
+
 import org.bson.Document;
 import org.bson.types.ObjectId;
 import org.eclipse.paho.client.mqttv3.*;
@@ -12,16 +14,9 @@ import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 public class MongoToMQTT {
-    static ObjectId objectId1 = null; // Resetting objectId for each iteration
+    static ObjectId objectId1 = null;
     static ObjectId objectId2 = null;
     
-    public static ObjectId convertString(String message) {
-        String[] splitedMessage = message.trim().split(",");
-        String values = splitedMessage[0].split(": ")[2];
-        String cleanedString = values.substring(1, values.length()-2);
-        return new ObjectId(cleanedString);
-    }
-
     public static void main(String[] args) throws InterruptedException {
         // MongoDB Configuration
         MongoClient mongoClient = new MongoClient("localhost", 27017);
@@ -30,6 +25,14 @@ public class MongoToMQTT {
         // MongoDB Collections
         MongoCollection<Document> collection1 = database.getCollection("medicoesTemperatura");
         MongoCollection<Document> collection2 = database.getCollection("medicoesPassagem");
+        MongoCollection<Document> bookmarks = database.getCollection("bookmarksObjectID");
+
+        //Init bookmarks
+        bookmarks.drop();
+        Document bookmarkTemperatura = new Document("collectionName", "medicoesTemperatura").append("lastProcessedId", null);
+        bookmarks.insertOne(bookmarkTemperatura);
+        Document bookmarkPassagem = new Document("collectionName", "medicoesPassagem").append("lastProcessedId", null);
+        bookmarks.insertOne(bookmarkPassagem);
 
         // MQTT Configuration
         String broker = "tcp://broker.mqtt-dashboard.com:1883";
@@ -43,10 +46,9 @@ public class MongoToMQTT {
                 MqttClient mqttClient = new MqttClient(broker, clientId);
                 mqttClient.connect();
                 
-                
                 // Submitting tasks to the executor service for concurrent execution
-                Future<ObjectId> future1 = executor.submit(() -> extractAndPublish(collection1, objectId1, mqttClient, "pisid_grupo2_joaosilva_temperatura"));
-                Future<ObjectId> future2 = executor.submit(() -> extractAndPublish(collection2, objectId2, mqttClient, "pisid_grupo2_joaosilva_passagem"));
+                Future<ObjectId> future1 = executor.submit(() -> extractAndPublish(collection1, bookmarks, objectId1, mqttClient, "pisid_grupo2_joaosilva_temperatura", "medicoesTemperatura"));
+                Future<ObjectId> future2 = executor.submit(() -> extractAndPublish(collection2, bookmarks, objectId2, mqttClient, "pisid_grupo2_joaosilva_passagem", "medicoesPassagem"));
 
                 objectId1 = future1.get(); // Get the returned objectId from the future
                 objectId2 = future2.get();
@@ -70,7 +72,7 @@ public class MongoToMQTT {
     }
     
     // Method to extract documents from collection, publish to MQTT, and update objectId
-    private static ObjectId extractAndPublish(MongoCollection<Document> collection, ObjectId objectId, MqttClient mqttClient, String topic) {
+    private static ObjectId extractAndPublish(MongoCollection<Document> collection, MongoCollection<Document> bookmarks, ObjectId objectId, MqttClient mqttClient, String topic, String collectionName) {
         Document query = new Document();
         if (objectId != null) {
             query.append("_id", new Document("$gt", objectId));
@@ -87,6 +89,9 @@ public class MongoToMQTT {
                 e.printStackTrace();
             }
             objectId = doc.getObjectId("_id");
+            Document bookmarkQuery = new Document("collectionName", collectionName);
+            Document bookmarkUpdate = new Document("$set", new Document("lastProcessedId", objectId));
+            bookmarks.updateOne(bookmarkQuery, bookmarkUpdate, new UpdateOptions().upsert(true));
         }
         return objectId;
     }
